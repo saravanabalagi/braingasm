5d789e27be80c172e3ecdbaf342b40df66efb512
zekedran <saravanabalagi@hotmail.com>
2016-03-04 13:17:30 +0530

    Added Utils

---
+++ app/src/main/java/app/braingasm/in/braingasm/utils/DateUtils.java
@@ -0,0 +1,26 @@
+package app.braingasm.in.braingasm.utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Created by Zeke on Oct 27 2015.
+ */
+public class DateUtils {
+
+    public static String railsDateToLocalTime(String railsUtcDateFormat) {
+        try {
+            DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+            utcDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date date  = utcDateFormat.parse(railsUtcDateFormat);
+            DateFormat istDateFormat = new SimpleDateFormat("MMM dd hh:mm aa", Locale.getDefault());
+            istDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
+            return istDateFormat.format(date);
+        } catch (ParseException e) { e.printStackTrace(); return null; }
+    }
+
+}
---
+++ app/src/main/java/app/braingasm/in/braingasm/utils/NumberUtils.java
@@ -0,0 +1,20 @@
+package app.braingasm.in.braingasm.utils;
+
+/**
+ * Created by Zeke on Sep 04 2015.
+ */
+public class NumberUtils {
+
+    public static String getCurrencyFormat(float f) { return String.format("%.2f",f); }
+    public static String getCurrencyFormat(double d) { return String.format("%.2f",d); }
+    public static boolean isInteger(String string) {
+        if (string == null) { return false; }
+        if (string.length() == 0) { return false; }
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if ( c < '0' || c > '9') { return false; }
+        }
+        return true;
+    }
+
+}
